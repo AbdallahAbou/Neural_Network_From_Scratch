16f57c9d46de53cd9934f14d784bf8cfece37491
abdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-07-28 22:18:54 +0200

    Load data to training

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -1,13 +1,13 @@
 from src.models.neural_network import NeuralNetwork
-from src.data.data_loader import load_data
+from src.data.data_loader import sine
 
 def train():
-    X, y = load_data()
+    X, y = sine.create_data()
     model = NeuralNetwork()
     
     for epoch in range(10000):
         loss = model.forward(X, y)
-        model.backward(X, y)
+        model.backward(y)
 
         if epoch % 100 == 0:
             print(f'Epoch {epoch}, Loss: {loss}')
